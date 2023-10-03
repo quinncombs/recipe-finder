b1fc16355dfa327016c35b02244335d1ccb10cc0
Abubakar50C <abubakara2005@gmail.com>
2023-10-02 21:29:24 -0400

    updated tailwind.config.js

---
+++ assets/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    colors: {
+      
+      'gray-light': '#F5F7FA',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
